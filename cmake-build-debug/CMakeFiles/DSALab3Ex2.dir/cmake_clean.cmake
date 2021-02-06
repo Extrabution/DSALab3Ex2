aff53b555c096712f4c3da509ef4174ba7e26bdb
Extrabution <super.vanya-inchin-2002@ya.ru>
2021-02-06 17:04:16 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DSALab3Ex2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSALab3Ex2.dir/main.cpp.obj"
+  "DSALab3Ex2.exe"
+  "DSALab3Ex2.exe.manifest"
+  "DSALab3Ex2.pdb"
+  "libDSALab3Ex2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSALab3Ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
